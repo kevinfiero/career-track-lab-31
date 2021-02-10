614a15b3a813bef8730d69b6de1d61a18c8ccc68
kevinfiero <kjf46@cornell.edu>
2021-02-09 20:30:43 -0800

    mock tests

--- src/pages/detailPage/DetailPage.test.jsx
+++ src/pages/detailPage/DetailPage.test.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { render, screen, waitFor, cleanup } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { act } from 'react-dom/test-utils';
+import characterDetailApiResponse from '../../fixtures/detailCharacter.json';
 import DetailPage from './DetailPage';
 
+const server = setupServer(
+  rest.get('https://hey-arnold-api.herokuapp.com/api/v1/characters/5da237699734fdcb7bef8f51', (req, res, ctx) => {
+    return res(ctx.json(characterDetailApiResponse));
+  })
+);
+
 describe('DetailPage Container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
   afterEach(() => cleanup());
   it('displays a loading screen then a list of characters', async() => {
-    render(
-      <DetailPage 
-        match={{ params: { id: '5f5fb4bbbfd05c2aed82e462' } }}
-      />
-    );
+    await act(() => {
+      render(<DetailPage match={{ params: { id: '5da237699734fdcb7bef8f51' } }}/>);
 
-    const ul = screen.getByText('Loading');
-
-    return waitFor(() => {
-      expect(ul).not.toBeEmptyDOMElement();
+      const ul = screen.getByText('Loading');
+    
+      return waitFor(() => {
+        expect(ul).not.toBeEmptyDOMElement();
+      });
     });
   });
 });
--- src/pages/listPage/ListPage.test.jsx
+++ src/pages/listPage/ListPage.test.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { render, screen, waitFor, cleanup } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { act } from 'react-dom/test-utils';
+import charactersApiResponse from '../../fixtures/characters.json';
 import ListPage from './ListPage';
 
+const server = setupServer(
+  rest.get('https://hey-arnold-api.herokuapp.com/api/v1/characters/', (req, res, ctx) => {
+    return res(ctx.json(charactersApiResponse));
+  })
+);
+
 describe('ListPage Container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+  
   afterEach(() => cleanup());
   it('displays a loading screen then a list of characters', async() => {
-    render(<ListPage />);
+    await act(() => {
+      render(<ListPage />);
 
-    const ul = screen.getByText('Loading');
+      const ul = screen.getByText('Loading');
 
-    return waitFor(() => {
-      expect(ul).not.toBeEmptyDOMElement();
+      return waitFor(() => {
+        expect(ul).not.toBeEmptyDOMElement();
+      });
     });
   });
 });
