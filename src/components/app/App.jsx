39b38865cb36303673e64e534ce69c9ed5f20482
kevinfiero <kjf46@cornell.edu>
2021-02-10 21:51:35 -0800

    dark mode

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ListPage from '../../pages/listPage/ListPage';
 import DetailPage from '../../pages/detailPage/DetailPage';
 import Header from '../header/Header';
+import './App.css';
+
+export const ThemeContext = React.createContext();
 
 export default function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+  function handleTheme() {
+    setDarkTheme(prevDarkTheme => !prevDarkTheme);
+  }
+
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ListPage} />
-        <Route path="/:id" component={DetailPage} />
-      </Switch>
-    </Router>
+    <section>
+      <ThemeContext.Provider value={darkTheme}>
+        <Router>
+          <Header onClick = {handleTheme} />
+          <Switch>
+            <Route exact path="/" component={ListPage} />
+            <Route path="/:id" component={DetailPage} />
+          </Switch>
+        </Router>
+      </ThemeContext.Provider>
+    </section>
+
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { ThemeContext } from '../app/App';
+import './Header.css';
 
-const Header = () => {
+const Header = ({ onClick }) => {
+  const darkTheme = useContext(ThemeContext);
+  const themeStyles = { backgroundColor: darkTheme ? 'white' : '#333' };
 
   return (  
-    <Link to="/" exact>
-      <button>Home</button>
-    </Link>
+    <div style={themeStyles}>
+      <button onClick={onClick}>Toggle</button>
+      <Link to="/" exact>
+        <button>Home</button>
+      </Link>
+    </div>
+
   );
 
+};
+
+Header.propTypes = {
+  onClick: PropTypes.func.isRequired
 };
 
 export default Header;
